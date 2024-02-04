8c720ea4e4ceeb923197539ca443d86dcc2b6adb
Skutin Artem <Skutinartem5@gmail.com>
2024-02-04 21:06:01 +0300

    Добавили задачу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Напишите задачу каторая принимает на вход
+// трёх значное число и удаляет вторую цифру этого числа
+
+int number = 256;
+int a = number / 100;
+int b = number % 10;
+int result = a * 10 + b;
+
+Console.WriteLine($"В числе {number} => {result}");
