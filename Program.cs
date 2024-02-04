dbbd229b7087148b3ad781f1be9db8ac38bbb7bc
Skutin Artem <Skutinartem5@gmail.com>
2024-02-04 21:58:10 +0300

    Добавил еще задачку

--- Program.cs
+++ Program.cs
@@ -12,9 +12,26 @@
 // Напишите програму которая выводит на вход трёх значное число 
 // и возводит вторую цифру в степень равную третий цифре
 
-int number = 925;
-int a = number / 10 % 10;
-int b = number % 10;
-int result = (int)Math.Pow(a, b);
+// int number = 925;
+// int a = number / 10 % 10;
+// int b = number % 10;
+// int result = (int)Math.Pow(a, b);
 
-Console.WriteLine($"{a} ^ {b} = {result}");
+// Console.WriteLine($"{a} ^ {b} = {result}");
+
+//  Напишите програму которая выводит третью
+// цифру с конца, или сообщает что 3 цифры нет
+// 456
+// 1234
+// 12 третий цифры нет
+
+int number = 932654;
+if (number >= 100)
+{
+    int a = (number / 100) % 10;
+    Console.WriteLine($"Третья цифра с конца {a} ");
+}
+else
+{
+    Console.WriteLine("третий цифры нет");
+}
