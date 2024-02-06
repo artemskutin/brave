53c25a98f5a3c9dd2fb0a60b247c406f9d0aa1c8
Skutin Artem <Skutinartem5@gmail.com>
2024-02-06 21:48:49 +0300

    Добавили задачу

--- Program.cs
+++ Program.cs
@@ -25,13 +25,66 @@
 // 1234
 // 12 третий цифры нет
 
-int number = 932654;
-if (number >= 100)
+// int number = 932654;
+// if (number >= 100)
+// {
+//     int a = (number / 100) % 10;
+//     Console.WriteLine($"Третья цифра с конца {a} ");
+// }
+// else
+// {
+//     Console.WriteLine("третий цифры нет");
+// }
+
+
+//  Запишите програму которая будет принимать на вход два числа 
+//  и выводить является ли второе число кратным первому
+//  если нет то выводит остаток от деления 
+
+// int a = 6;
+// int b = 3;
+
+// if (a % b == 0)
+// {
+//     Console.WriteLine($"{a} кратно (делится без остатка)");
+// }
+// else
+// {
+//     Console.WriteLine($"остаток от {a} / {b} = {a % b}");
+// }
+
+// //  Проверка на четность 
+// int number = 6;
+//         if ((number % 2) == 0)
+//         {
+//             Console.WriteLine($"четное");
+//         }
+//         else
+//         {
+//             Console.WriteLine($"нечетное");
+//         }
+
+
+// int number = 6;
+// int a = 1;
+// while (a <= number)
+// {
+// if (a % 2 == 0)
+// {
+//     Console.Write(a + " ");
+// }
+//     a++;
+// }
+
+
+
+double CalculateFormula (int b, int c, int d, int g)
 {
-    int a = (number / 100) % 10;
-    Console.WriteLine($"Третья цифра с конца {a} ");
+    double  numenator = b * c;
+    int  deminator = d + g;
+    double result = numenator / deminator;
+    return  result;
 }
-else
-{
-    Console.WriteLine("третий цифры нет");
-}
+double result = CalculateFormula (1, 2, 3, 4);
+
+Console.WriteLine(result);
